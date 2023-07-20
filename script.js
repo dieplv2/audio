c2b46f83ec4e011245557c19fc487b3ed6067b24
dieplv2 <dieplv2@gmail.com>
2023-07-20 19:47:11 +0700

    fix Css

--- script.js
+++ script.js
@@ -5,33 +5,15 @@ let navbar = $$(".nav-list li");
 
 let icon = $(".icon");
 
-icon.onclick = function () {
+icon.addEventListener("click", function(){
     let x = $("#navtop");
-
     if (x.className === "nav-list") {
-        console.log('pass');
         x.className += " reponsive";
     } else {
         x.className = "nav-list";
     }
-}
+})
 
-
-// function myPlay() {
-//     if(myMp3.play()){
-//         $(".play").style.display = 'none';
-//         $(".pause").style.display = 'block';
-//     }
-
-// }
-
-// function myPause() {
-
-//     console.log($(".play").style.display = 'block');
-//     if(myMp3.pause());
-//         $(".pause").style.display = 'none';
-
-// }
 
 const App = {
     // Menu top
@@ -201,7 +183,7 @@ const App = {
                 <button class="active">${btn.number}${btn.title}</button>
             `
         })
-        $(".btn-slider").innerHTML = btnTop.join("");
+        $(".btntop").innerHTML = btnTop.join("");
 
         // render Categories
         const cat = this.categories.map(function(category){
